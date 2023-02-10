d5ef544485e1331b3e323c609215dde4d3482445
michaljano <michal.dutka0@gmail.com>
2023-02-10 08:51:49 +0100

    kapusta cikoky

---
+++ kapustinica.py
@@ -0,0 +1,27 @@
+n = int(input())
+text = input()
+newText = ""
+pocetT = 0
+pocetV = 0
+first = True
+vIsWaiting = 0
+for i in text:
+    if first:
+        if i == "T":
+            newText += "VT"
+        elif i == "V":
+            vIsWaiting += 1
+            newText += "V"
+        first = False
+        continue
+    if i == "T":
+        if vIsWaiting == 0:
+            newText += "VT"
+        else:
+            vIsWaiting -= 1
+            newText += "T"
+    elif i == "V":
+        newText += "V"
+        vIsWaiting += 1
+
+print(newText+"T"*vIsWaiting)
